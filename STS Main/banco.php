0c55c453dacb42a81529a7f28d04cd53373025e9
Ander <33981822+ander-lima@users.noreply.github.com>
2025-06-18 19:39:43 -0300

    Adição de configuração de conjunto de carácteres para conexão no banco. + Divisão de arquivos de configuração para teste de segurança com arquivos sensiveis.

--- STS Main/banco.php
+++ STS Main/banco.php
@@ -1,11 +1,13 @@
 <?php 
-// Variaveis 
-$host = "localhost";
-$usuario = "root";
-$senha = "";
-$banco = "clientes";
+//Detectar se está rodando local ou online para conseguir versionar sem riscos de segurança
+if ($_SERVER['HTTP_HOST'] === 'localhost') {
+    include 'config_local.php';
+} else {
+    include 'config_prod.php';
+}
 
-//Conexão com banco
+
+//Conexão com banco - Vai puxar o especifico do ambiente
 $conexao = mysqli_connect($host, $usuario, $senha, $banco);
 
 //Teste de Conexão
@@ -13,4 +15,5 @@ if (!$conexao) {
     die ("Falha na conexão:" . mysqli_connect_error());
 }
 
+mysqli_set_charset($conexao, 'utf8mb4');
 ?>
