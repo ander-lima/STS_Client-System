54d7e1759c64f5f2ef7d1a902b9ec9d33b8bf431
Ander <33981822+ander-lima@users.noreply.github.com>
2025-08-11 21:03:36 -0300

    Adição página de KanBan do Projeto

---
+++ STS Main/kanban_att.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="imagens/fear.ico" type="image/x-icon">
+    <title>KanBan do Projeto</title>
+</head>
+<body>
+    <main>
+        <h1>Essa página servirá para atualizar o quadro Kanban do projeto</h1>
+        <h2>Irei atualizar inicialmente com prints até encontrar forma mais profissional</h2>
+
+        <p>11-08-2025   </p>
+        <img src="imagens/kanban_att/kanban_11.08-25.png" alt="11-08-2025">
+    </main>
+</body>
+</html>
