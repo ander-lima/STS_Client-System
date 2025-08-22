8de518b10ba759e3a3d6833d92373499d1cae7e2
Ander <33981822+ander-lima@users.noreply.github.com>
2025-08-22 20:18:06 -0300

    Só comentários para pensar na lógica. Senti que hoje precisava descansar

--- index.php
+++ index.php
@@ -2,14 +2,15 @@
 // index.php será o router. Ele receberá todas as requisições e decidirá qual controller chamar. 
 // Vantagem: Organiza código; Deixa Url mais bonita (só aparece a rota chamada e não o nome do arquivo)
 
-$rota = ($_GET['rota'] ?? 'login') . "Controller.php"; // Define como variável rota por padrão o valor definido na URL. Ex: "index.php?rota=login" contém rota=login, mas também é definido por padrão se estiver vazio
+$rota = $_GET['rota'] ?? 'login'; // Define como variável rota por padrão o valor definido na URL. Ex: "index.php?rota=login" contém rota=login, mas também é definido por padrão se estiver vazio
 
 switch($rota) { //Menu de decisões
 
     case 'login':
 
-        require "controllers/$rota"; // Carrega o script de loginController para usar suas classes e métodos
- 
+        require "controllers/loginController.php"; // Carrega o script de loginController para usar suas classes e métodos
+        $controller = new loginController();
+        $controller->login();
 
         break;
 
